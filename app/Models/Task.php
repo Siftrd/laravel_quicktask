6594608d43ceefce1e623f45a1c6ba585f6fb2ca
Siftrd <rioxvee@gmail.com>
2023-08-01 11:58:05 +0700

    Update Task.php

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,7 +6,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\models\User;
-
 class Task extends Model
 {
     use HasFactory;
